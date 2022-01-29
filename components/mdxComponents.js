37204191943320d53d24b3f66cc6261a3eedbe33
Jeremy Richardson <jeremy.richrdson@gmail.com>
2022-01-28 22:08:38 -0600

    fix: play with size of image to see if it loads a smaller version

--- components/mdxComponents.js
+++ components/mdxComponents.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const mdxComponents = {
   img: (props) => (
     // height and width are part of the props, so they get automatically passed here with {...props}
-    <Image {...props} layout="responsive" loading="lazy" alt="" sizes="50vw" />
+    <Image {...props} layout="responsive" loading="lazy" alt="" sizes="25vw" />
   ),
 };
 export default mdxComponents;
