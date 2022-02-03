ebb9cb981c5906bb4b2f01adc40c9f22aea12a0b
Jeremy Richardson <jrichardson@thought-dev.com>
2022-02-02 21:49:39 -0600

    feat: add about, tech icons

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,15 @@ import rehypePrism from "rehype-prism-plus";
 import remarkGfm from "remark-gfm";
 import imageSize from "rehype-img-size";
 import { ErrorBoundary } from "../components/ErrorBoundary";
+import { DiReact, DiSass } from "react-icons/Di";
+import {
+  IoLogoJavascript,
+  IoLogoHtml5,
+  IoLogoCss3,
+  IoLogoNodejs,
+} from "react-icons/io5";
+import { FaAws, FaGitAlt } from "react-icons/Fa";
+import { SiSwagger } from "react-icons/Si";
 
 export default function Home({ posts }) {
   return (
@@ -26,6 +35,10 @@ export default function Home({ posts }) {
           <div className="hero-container">
             <div className="hero-content">
               <h1 className="title">Hi, I&apos;m Jeremy Richardson</h1>
+              <p>
+                I&apos;m a full stack developer building software solutions that
+                increase user productivity
+              </p>
             </div>
             <div className="hero-image hero-section">
               <div className="profile-picture-border">
@@ -41,6 +54,44 @@ export default function Home({ posts }) {
           </div>
         </div>
         <main className="main">
+          <h2>About</h2>
+          <p>
+            My current mission with{" "}
+            <a href="https://www.thought-dev.com">Thought Development</a> is to
+            lead a team as we transform ON-Air Pro&mdash;a legacy FileMaker
+            application for the broadcast industry&mdash;into a modern web
+            application using node, react, and AWS.{" "}
+          </p>
+          <p>
+            We have to support the existing customer base who continues to use
+            the legacy product along-side the new product, which means
+            we&apos;re using the FileMaker data API to retrieve data, transform
+            it, and serve it to the client application through our newly defined
+            REST API.
+          </p>
+          <p>
+            My past had led me through a number of creative fields including
+            film and television production, audio production as both a musician
+            and engineer, as well as live theatrical sound and lighting.
+          </p>
+          <p>
+            Each of those ventures always led me back to the web where the
+            innovation in technology attracted me time and time again. I love
+            building tools that people love to use, especially in the creative
+            and entertainment realms.
+          </p>
+          <h2>Tech</h2>
+          <div className="tech-logos">
+            <DiReact size="3em" title="React" />
+            <IoLogoJavascript size="3em" title="Javascript" />
+            <IoLogoHtml5 size="3em" title="HTML5" />
+            <IoLogoCss3 size="3em" title="CSS3" />
+            <DiSass size="3em" title="Sass" />
+            <IoLogoNodejs size="3em" title="NodeJS" />
+            <FaAws size="3em" title="AWS" />
+            <SiSwagger size="3em" title="OpenAPI/Swagger" />
+            <FaGitAlt size="3em" title="Git" />
+          </div>
           <h2>Posts</h2>
           <article className="container prose prose-sm md:prose">
             {posts.map((post) => (
