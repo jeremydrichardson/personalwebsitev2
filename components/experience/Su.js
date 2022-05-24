56b38aa7872077154b53962d70c23cbd8617b341
Jeremy Richardson <jrichardson@thought-dev.com>
2022-05-24 14:12:36 -0500

    feat: add timeline to experience page

---
+++ components/experience/Su.js
@@ -0,0 +1,67 @@
+export function Su() {
+  return (
+    <>
+      <h2 className="company-name">
+        Shiloh University&nbsp;&nbsp;&nbsp;
+        <span className="position-title">Educational Systems Manager</span>
+      </h2>
+      <p className="position-date-range">Jan 2019 - May 2020</p>
+
+      <ul>
+        <li>
+          Designed and implemented lead generation campaign (Facebook Ads,
+          Google Ads) saving university $100K+ of consulting fees in marketing
+          and recruitment efforts
+        </li>
+        <li>
+          Building landing pages and programmed automated workflow (WordPress,
+          Campaign Monitor, HubSpot)
+        </li>
+        <li>
+          Created a drip campaign designed with responsive emails using MJML
+        </li>
+        <li>
+          Refactored entire shilohuniversity.edu WordPress site to improve
+          administrator experience including php templates, CSS styles, and
+          implemented modern tooling workflow (gulp, sass)
+        </li>
+        <li>
+          Added custom post types for better organization of data (faculty,
+          programs, courses, etc.)
+        </li>
+        <li>Mobile improvements including better menus, responsive images</li>
+        <li>
+          Built custom Gutenberg blocks and moved all content to use blocks
+        </li>
+        <li>
+          Automated email communications for admissions and registrar functions
+          using Campus Café SIS saving hundreds of hours of manual Outlook based
+          emailing
+        </li>
+        <li>
+          Implemented CRM (HubSpot) to manage leads and prospects streamlining
+          recruitment efforts including website chat, Office 365 integration,
+          contact synchronization, and form data collection
+        </li>
+        <li>
+          Designed and implemented systems for storing all student and other
+          records digitally
+        </li>
+        <li>
+          Acted as Registrar to understand requirements while designing and
+          implementing series of technology and procedural solutions to
+          streamline for maximum efficiency with minimal personnel resources
+        </li>
+        <li>
+          Acted as Admissions to understand requirements while designing and
+          implementing series of technology and procedural solutions to
+          streamline for maximum efficiency with minimal personnel resources
+        </li>
+        <li>
+          Trained and mentored new registrar and admissions officer making them
+          effective in a short period of time
+        </li>
+      </ul>
+    </>
+  );
+}
