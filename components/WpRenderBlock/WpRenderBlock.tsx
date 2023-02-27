2f8dc49145fc4b17d2af9163bed2baa7c825e3f1
Jeremy Richardson <jeremy.richrdson@gmail.com>
2023-02-26 22:22:10 -0600

    feat: add youtube embeds

--- components/WpRenderBlock/WpRenderBlock.tsx
+++ components/WpRenderBlock/WpRenderBlock.tsx
@@ -7,6 +7,10 @@ interface WpRenderBlockProps {
   markupProps?: MarkupProps;
 }
 
+interface CoreEmbedAttributes {
+  url?: string;
+}
+
 export const WpRenderBlock = ({ block, markupProps }: WpRenderBlockProps) => {
   if (block.blockName === "core/list") {
     const listItems = block.innerBlocks.map((innerBlock) => {
@@ -18,6 +22,23 @@ export const WpRenderBlock = ({ block, markupProps }: WpRenderBlockProps) => {
 
   if (block.blockName === "core/code") {
     return <Code block={block} />;
+  }
+
+  if (block.blockName === "core/embed") {
+    const coreAttributes = block.attrs as CoreEmbedAttributes;
+    if (coreAttributes.url && /youtube.com/i.test(coreAttributes.url)) {
+      return (
+        <iframe
+          width="560"
+          height="315"
+          src={coreAttributes.url}
+          title="YouTube video player"
+          // frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          // allowfullscreen
+        ></iframe>
+      );
+    }
   }
 
   return (
