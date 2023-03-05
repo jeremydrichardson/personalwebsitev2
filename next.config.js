e429aef5265f10d33306b25242d56444f8806b71
Jeremy Richardson <jeremy.richrdson@gmail.com>
2023-03-05 12:03:50 -0600

    refactor: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,30 +1,15 @@
-import mdx from "@next/mdx";
-import remarkFrontmatter from "remark-frontmatter";
-
-const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [remarkFrontmatter],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
-export default withMDX({
+const config = {
   // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "jeremyrichardsonhome.files.wordpress.com",
-        port: "",
         pathname: "/**",
       },
     ],
   },
-});
+};
+export default config;
